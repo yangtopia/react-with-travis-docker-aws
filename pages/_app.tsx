79959273dcab708a655a9aa4d94e6d3dd1f0381c
yangtopia <yangtopia@gmail.com>
2019-08-24 15:22:57 +0900

    head 추가 및 static 파일 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import App, { Container } from 'next/app';
+import Head from 'next/head';
 import { Provider } from 'react-redux';
 import withRedux, { AppProps } from 'next-redux-wrapper';
 import configureStore from '../src/redux/configureStore';
-import '../style.css';
+
+import 'moment/locale/ko';
+import '../styles/global.css';
 
 class MyApp extends App<AppProps> {
   render() {
@@ -11,6 +14,13 @@ class MyApp extends App<AppProps> {
 
     return (
       <Container>
+        <Head>
+          <title>셀프소개팅</title>
+          <link rel="apple-touch-icon" sizes="180x180" href="/static/_root/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/static/_root/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/static/_root/favicon-16x16.png" />
+          <link rel="manifest" href="/static/_root/site.webmanifest" />
+        </Head>
         <Provider store={store}>
           <Component {...pageProps} />
         </Provider>
